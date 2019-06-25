fd65032d61f717116231e4b8458c10ab611943b3
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-24 22:16:43 -0400

    Vowel and String utils done

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -1,25 +1,39 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author leon on 09/12/2018.
  */
 public class StringUtils {
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+        return str.substring(0,indexToCapitalize)+Character.toUpperCase(str.charAt(indexToCapitalize))+str.substring(indexToCapitalize+1);
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+        return characterToCheckFor.equals(baseString.charAt(indexOfString));
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
+        Set<String> set = buildSubstringSet(string);
+        return set.toArray(new String[0]);
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return null;
+        Set<String> set = buildSubstringSet(input);
+        return set.size();
     }
+
+    private static Set<String> buildSubstringSet(String input) {
+        Set<String> set = new LinkedHashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 1; j < input.length() - i+1; j++)
+                set.add(input.substring(i, i + j));
+        }
+        return set;
+    }
+
 }
--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -5,19 +5,23 @@ package rocks.zipcode.io.quiz3.fundamentals;
  */
 public class VowelUtils {
     public static Boolean hasVowels(String word) {
-        return null;
+        for(char c:word.toCharArray())if(isVowel(c))return true;
+        return false;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        for (int i = 0; i < word.length(); i++) {
+            if(isVowel(word.charAt(i)))return i;
+        }
+        return -1;
     }
 
-
     public static Boolean startsWithVowel(String word) {
-        return null;
+        return isVowel(word.charAt(0));
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        String s = ""+Character.toLowerCase(character);
+        return s.matches("[aeiou]");
     }
 }
