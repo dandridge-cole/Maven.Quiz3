2c3657d77264a0fd7b6a1727fb5010da3bb4e7bd
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-24 22:32:03 -0400

    Pig Latin all tests passing

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -5,6 +5,17 @@ package rocks.zipcode.io.quiz3.fundamentals;
  */
 public class PigLatinGenerator {
     public String translate(String str) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        String[] words = str.split(" ");
+        for(int i=0;i<words.length-1;i++)builder.append(translateWord(words[i])).append(" ");
+        builder.append(translateWord(words[words.length-1]));
+        return builder.toString();
+    }
+
+    private String translateWord(String str){
+        if(!VowelUtils.hasVowels(str))return str+"ay";
+        if(VowelUtils.startsWithVowel(str))return str+"way";
+        Integer firstVowel = VowelUtils.getIndexOfFirstVowel(str);
+        return str.substring(firstVowel)+str.substring(0,firstVowel)+"ay";
     }
 }
