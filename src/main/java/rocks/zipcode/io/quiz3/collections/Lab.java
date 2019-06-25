8e939c58f70b4d864bf6e1b3dc3e0628fb106d96
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-24 21:14:14 -0400

    Collections passing all tests.

--- src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
@@ -4,21 +4,25 @@ package rocks.zipcode.io.quiz3.collections;
  * @author leon on 10/12/2018.
  */
 public class Lab {
+    LabStatus ls;
+    String ln;
     public Lab() {
         this(null);
     }
 
     public Lab(String labName) {
+        this.ln=labName;
     }
 
     public String getName() {
-        return null;
+        return ln;
     }
 
     public void setStatus(LabStatus labStatus) {
+        this.ls = labStatus;
     }
 
     public LabStatus getStatus() {
-        return null;
+        return ls;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.io.quiz3.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,24 +8,48 @@ import java.util.List;
  */
 public class Student {
 
+    public List<Lab> labs;
+
     public Student() {
-        this(null);
+        this(new ArrayList<>());
     }
 
     public Student(List<Lab> labs) {
+        this.labs = labs;
     }
 
     public Lab getLab(String labName) {
-        return null;
+        for(Lab lab:this.labs)if(lab.getName().equals(labName))return lab;
+        Lab lab = new Lab(labName);
+        labs.add(lab);
+        return lab;
     }
 
     public void setLabStatus(String labName, LabStatus labStatus) {
+        Lab lab = getLab(labName);
+        if(lab.getStatus()==null)throw(new UnsupportedOperationException());
+        if(lab.getStatus().equals(LabStatus.PENDING)){
+            lab.setStatus(labStatus);
+            return;
+        }
+        else throw(new UnsupportedOperationException());
     }
 
     public void forkLab(Lab lab) {
+        lab.setStatus(LabStatus.PENDING);
+        for(int i = 0; i<labs.size();i++) if(this.labs.get(i).equals(lab)) return;
+        this.labs.add(lab);
     }
 
     public LabStatus getLabStatus(String labName) {
-        return null;
+        return getLab(labName).getStatus();
+    }
+
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        for(int i=this.labs.size()-1;i>0;i--)
+            builder.append(String.format("%s > %s\n",this.labs.get(i).getName(),this.labs.get(i).getStatus()));
+        builder.append(String.format("%s > %s",this.labs.get(0).getName(),this.labs.get(0).getStatus()));
+        return builder.toString();
     }
 }
